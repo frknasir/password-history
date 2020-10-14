e21d83bc674d38c4d3c0826318a378f26f7b7b9f
Faruk Nasir <frknasir@yahoo.com>
2020-10-14 10:10:17 +0100

    added password history check rule

---
+++ src/Rules/NotInRecentPasswordHistory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace StarfolkSoftware\PasswordHistory\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Auth;
+
+class NotInRecentPasswordHistory implements Rule {
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        //get the last 5 password history of the loggedin user
+        $prevPasswords = Auth::user()->previousPasswords()->take(5);
+
+        //loop through and make sure none is the same with the 
+        //has of the will-be password
+        $passwordCollection = collect($prevPasswords);
+        
+        return !($passwordCollection->contains(function ($val, $key) use ($value) {
+            return $key == 'password' && $val == $value;
+        }));
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must not be the same with recent passwords';
+    }
+}
--- src/Traits/HasPasswordHistory.php
+++ src/Traits/HasPasswordHistory.php
@@ -10,11 +10,4 @@ trait HasPasswordHistory {
     public function previousPasswords(): HasMany {
         return $this->hasMany(PasswordHistory::class, 'user_id');
     }
-
-    public function savePasswordToHistory($hashedPassword) {
-        return PasswordHistory::create([
-            'user_id' => Auth::user()->id,
-            'password' => $hashedPassword
-        ]);
-    }
 }
